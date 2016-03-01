f1c140c612afdd35203f24f7b446252122f2b27c
Seth Battis <sethbattis@stmarksschool.org>
2016-03-01 17:27:53 -0500

    Namespacing
    Huh. Right: if the function is called by the JQuery `$().each()` method, then the object that `this` refers to must be the object on which `each()` was called, not the original namespace of the function being called. So... easier to just redefine the dang array of colors every time.
    
    Also: let's just push out a default color using Canvas course colors API (that allows folks to customize the non-color blocks, for one thing, and it makes the color swatch match the basic calendar color).
    
    `manifest.xml` needs some attention -- need to figure out what the trigger really is for calling this.

--- color-schedule.js.php
+++ color-schedule.js.php
@@ -9,32 +9,30 @@ $colorSchedule = 1530;
 ?>
 
 var canvashack = {
-	color: {
-		'red': '#<?= sm::get(sm::RED_BLOCK)->light()->value() ?>',
-		'orange': '#<?= sm::get(sm::ORANGE_BLOCK)->light()->value() ?>',
-		'yellow': '#<?= sm::get(sm::YELLOW_BLOCK)->light()->value() ?>',
-		'green': '#<?= sm::get(sm::GREEN_BLOCK)->light()->value() ?>',
-		'blue': '#<?= sm::get(sm::BLUE_BLOCK)->light()->value() ?>',
-		'plum': '#<?= sm::get(sm::PLUM_BLOCK)->light()->value() ?>',
-		'brown': '#<?= sm::get(sm::BROWN_BLOCK)->light()->value() ?>'
+	
+	colorBlockEach: function(i,e) {
+		var color = {
+			'red': '#<?= sm::get(sm::RED_BLOCK)->light()->value() ?>',
+			'orange': '#<?= sm::get(sm::ORANGE_BLOCK)->light()->value() ?>',
+			'yellow': '#<?= sm::get(sm::YELLOW_BLOCK)->light()->value() ?>',
+			'green': '#<?= sm::get(sm::GREEN_BLOCK)->light()->value() ?>',
+			'blue': '#<?= sm::get(sm::BLUE_BLOCK)->light()->value() ?>',
+			'plum': '#<?= sm::get(sm::PLUM_BLOCK)->light()->value() ?>',
+			'brown': '#<?= sm::get(sm::BROWN_BLOCK)->light()->value() ?>'
+		};
+		
+		var block = $(e).find('.fc-title').text().toLowerCase().substring(13);
+		
+		if (block in color) {
+			$(e).css('color', color[block]);
+			$(e).css('border-left-color', color[block]);
+			$(e).css('border-right-color', color[block]);
+			$(e).css('border-top-color', color[block]);
+			$(e).css('border-bottom-color', color[block]);
+		} /* else use whatever the default color of the color block schedule is... */
 	},
 	
-	colorize: function() {
-		$('.fc-event.group_course_<?= $colorSchedule ?>').each(function(i,e) {
-			var block = $(e).find('.fc-title').text().toLowerCase().substring(13);
-			if (block in canvashack.color) {
-				$(e).css('color', canvashack.color[block]);
-				$(e).css('border-left-color', canvashack.color[block]);
-				$(e).css('border-right-color', canvashack.color[block]);
-				$(e).css('border-top-color', canvashack.color[block]);
-				$(e).css('border-bottom-color', canvashack.color[block]);
-			} else {
-				$(e).css('color', 'lightgray');
-				$(e).css('border-left-color', 'lightgray');
-				$(e).css('border-right-color', 'lightgray');
-				$(e).css('border-top-color', 'lightgray');
-				$(e).css('border-bottom-color', 'lightgray');
-			}
-		});
+	colorBlock: function() {
+		$('.fc-event.group_course_<?= $colorSchedule ?>').each(this.colorBlockEach);
 	}
 };

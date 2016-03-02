16a260f2fbb5780bddc33172200489a386b8f621
Seth Battis <sethbattis@stmarksschool.org>
2016-03-02 12:58:20 -0500

    CSS
    Ah, right. *Model.* *View.* *Controller.* Let's put the CSS in a separate stylesheet. Makes it a lot easier to work with.
    
    Also, renaming to reflect the specificity of this solution for St. Mark's.

---
+++ color-schedule.css.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once(__DIR__ . '/common.inc.php');
+
+use smtech\StMarksColors as sm;
+
+foreach(sm::all() as $color) {
+	$color = strtolower($color);
+	echo "
+
+.stmarks-$color-event {
+	color: " . sm::get($color)->dark()->hex() . " !important;
+	background-color: " . sm::get($color)->light()->hex() . " !important;
+	border-color: " . sm::get($color)->dark()->hex() . " !important;
+}
+
+.stmarks-$color-agenda {
+	color: " . sm::get($color)->dark()->hex() . " !important;
+}
+	
+	";
+}
+	
+?>
--- color-schedule.js.php
+++ color-schedule.js.php
@@ -1,9 +1,7 @@
 <?php
 
-require_once(__DIR__ . '/common.inc.php');
+require_once __DIR__ . '/common.inc.php';
 
-use smtech\StMarksColors as sm;
-	
 ?>
 
 var canvashack = {
@@ -12,27 +10,14 @@ var canvashack = {
 		$(document).on('DOMNodeInserted', function(_event) {
 			var e = _event.target;
 			var events = $(e).find('.fc-event.group_course_<?= $pluginMetadata['color_schedule'] ?>');
+			var agenda = $(e).find('.ig-title.group_course_<?= $pluginMetadata['color_schedule'] ?>');
 			if (events.length > 0) {
-				$(events).each(function(i,e) {
-					var color = {
-						'red': '#<?= sm::get(sm::RED_BLOCK)->dark()->value() ?>',
-						'orange': '#<?= sm::get(sm::ORANGE_BLOCK)->dark()->value() ?>',
-						'yellow': '#<?= sm::get(sm::YELLOW_BLOCK)->dark()->value() ?>',
-						'green': '#<?= sm::get(sm::GREEN_BLOCK)->dark()->value() ?>',
-						'blue': '#<?= sm::get(sm::BLUE_BLOCK)->dark()->value() ?>',
-						'plum': '#<?= sm::get(sm::PLUM_BLOCK)->dark()->value() ?>',
-						'brown': '#<?= sm::get(sm::BROWN_BLOCK)->dark()->value() ?>'
-					};
-					
-					var block = $(e).find('.fc-title').text().toLowerCase().substring(13);
-					
-					if (block in color) {
-						$(e).css('color', color[block]);
-						$(e).css('border-left-color', color[block]);
-						$(e).css('border-right-color', color[block]);
-						$(e).css('border-top-color', color[block]);
-						$(e).css('border-bottom-color', color[block]);
-					} /* else use whatever the default color of the color block schedule is... */
+				$(events).each(function(i, e) {
+					$(e).addClass('stmarks-' + $(e).find('.fc-title').text().toLowerCase().substring(13).replace(/ /, '-') + '-event');
+				});
+			} else if (agenda.length > 0) {
+				$(agenda).each(function(i, e) {
+					$(e).addClass('stmarks-' + $(e).text().toLowerCase().trim().replace(/\s+/g,'-').substring(7) + '-agenda');
 				});
 			}
 		});

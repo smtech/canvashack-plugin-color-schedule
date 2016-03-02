6fabc38aeec459e00b53cfc7315e509c8700b46f
Seth Battis <sethbattis@stmarksschool.org>
2016-03-02 11:17:49 -0500

    on(DOMNodeInsert) is slow
    ...but it seems like the most pragmatic way of ensuring that colors are applied correctly as soon as possible.

--- color-schedule.js.php
+++ color-schedule.js.php
@@ -1,38 +1,40 @@
 <?php
 
-require_once('common.inc.php');
+require_once(__DIR__ . '/common.inc.php');
 
 use smtech\StMarksColors as sm;
-
-$colorSchedule = 1530;
 	
 ?>
 
 var canvashack = {
 	
-	colorBlockEach: function(i,e) {
-		var color = {
-			'red': '#<?= sm::get(sm::RED_BLOCK)->light()->value() ?>',
-			'orange': '#<?= sm::get(sm::ORANGE_BLOCK)->light()->value() ?>',
-			'yellow': '#<?= sm::get(sm::YELLOW_BLOCK)->light()->value() ?>',
-			'green': '#<?= sm::get(sm::GREEN_BLOCK)->light()->value() ?>',
-			'blue': '#<?= sm::get(sm::BLUE_BLOCK)->light()->value() ?>',
-			'plum': '#<?= sm::get(sm::PLUM_BLOCK)->light()->value() ?>',
-			'brown': '#<?= sm::get(sm::BROWN_BLOCK)->light()->value() ?>'
-		};
-		
-		var block = $(e).find('.fc-title').text().toLowerCase().substring(13);
-		
-		if (block in color) {
-			$(e).css('color', color[block]);
-			$(e).css('border-left-color', color[block]);
-			$(e).css('border-right-color', color[block]);
-			$(e).css('border-top-color', color[block]);
-			$(e).css('border-bottom-color', color[block]);
-		} /* else use whatever the default color of the color block schedule is... */
-	},
-	
-	colorBlock: function() {
-		$('.fc-event.group_course_<?= $colorSchedule ?>').each(this.colorBlockEach);
+	colorSchedule: function() {
+		$(document).on('DOMNodeInserted', function(_event) {
+			var e = _event.target;
+			var events = $(e).find('.fc-event.group_course_<?= $pluginMetadata['color_schedule'] ?>');
+			if (events.length > 0) {
+				$(events).each(function(i,e) {
+					var color = {
+						'red': '#<?= sm::get(sm::RED_BLOCK)->dark()->value() ?>',
+						'orange': '#<?= sm::get(sm::ORANGE_BLOCK)->dark()->value() ?>',
+						'yellow': '#<?= sm::get(sm::YELLOW_BLOCK)->dark()->value() ?>',
+						'green': '#<?= sm::get(sm::GREEN_BLOCK)->dark()->value() ?>',
+						'blue': '#<?= sm::get(sm::BLUE_BLOCK)->dark()->value() ?>',
+						'plum': '#<?= sm::get(sm::PLUM_BLOCK)->dark()->value() ?>',
+						'brown': '#<?= sm::get(sm::BROWN_BLOCK)->dark()->value() ?>'
+					};
+					
+					var block = $(e).find('.fc-title').text().toLowerCase().substring(13);
+					
+					if (block in color) {
+						$(e).css('color', color[block]);
+						$(e).css('border-left-color', color[block]);
+						$(e).css('border-right-color', color[block]);
+						$(e).css('border-top-color', color[block]);
+						$(e).css('border-bottom-color', color[block]);
+					} /* else use whatever the default color of the color block schedule is... */
+				});
+			}
+		});
 	}
 };
--- common.inc.php
+++ common.inc.php
@@ -1,5 +1,13 @@
 <?php
-	
-require_once(__DIR__ . '/../common.inc.php');
+
+require_once __DIR__ . '/../common.inc.php';
+
+if (file_exists(__DIR__ . '/manifest.xml')) {
+	$manifest = simplexml_load_string(file_get_contents(__DIR__ . '/manifest.xml'));
+}
+
+// FIXME if the manifest can't be loaded, this won't work either
+$pluginMetadata = new Battis\AppMetadata($sql, (string) $manifest->id);
+$pluginMetadata['color_schedule'] = 1530;
 
 ?>
